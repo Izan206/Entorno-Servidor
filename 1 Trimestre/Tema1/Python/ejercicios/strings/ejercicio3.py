913e8785e287c7206b8106e4b327e11de3685f15
izan.alvarezvarela <alvarezvarelaizan@gmail.com>
2025-09-27 09:49:40 +0200

    Ejercicios 3 y 4 de Strings subidos

--- 1 Trimestre/Tema1/Python/ejercicios/strings/ejercicio3.py
+++ 1 Trimestre/Tema1/Python/ejercicios/strings/ejercicio3.py
@@ -4,7 +4,7 @@ for i in range(len(cadena)):
     letra=cadena[i]
     if (letra=='a' or letra=='e' or letra=='i' or letra=='o' or letra=='u'):
         nuevaCadena+=letra
-    elif (letra.isnumeric or letra.isspace):
+    elif (letra.isnumeric() or letra.isspace()):
         nuevaCadena+=letra
     else:
         nuevaCadena=nuevaCadena
---
+++ 1 Trimestre/Tema1/Python/ejercicios/strings/ejercicio4.py
@@ -0,0 +1,4 @@
+cadena=input("Introduce una cadena: ")
+palabras=cadena.split()
+contadorPalabras=len(palabras)
+print(f"Hay {contadorPalabras} palabras en tu cadena")
